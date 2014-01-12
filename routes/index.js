2a73ff1c36b99bedd40d723be56fe501e43dc46c
Cristian Planas <me@cristianplanas.com>
2014-01-12 22:13:42 +0100

    Removing useless Redis calls

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,9 @@
  */
 
 exports.index = function(req, res){
-  var matches;
-  req.redisClient.set("playfulscore", "match");
-
-  req.redisClient.get("playfulscore", function(e, v){
-    matches = v; 
-    console.log(matches);
-    req.db.matches.find().toArray(function(error, matches){
-      if (error) return next(error);
-      res.render('index', { title: 'PlayfulScore',
-        matches: matches });
-      });
-    })
+  req.db.matches.find().toArray(function(error, matches){
+    if (error) return next(error);
+    res.render('index', { title: 'PlayfulScore',
+      matches: matches });
+  });
 };
