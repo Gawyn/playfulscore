7b0508da98bf1912cfea8184b61f82dd89d90306
Cristian Planas <me@cristianplanas.com>
2014-01-12 21:33:04 +0100

    Setting up Mongo database and matches collection

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var mongoskin = require ('mongoskin');
+
+var db = mongoskin.db('mongodb://localhost:27017/playfulscore?auto_reconnect', {safe:true});
 
 var app = express();
+app.use(function(req, res, next) {
+  req.db = {};
+  req.db.matches = db.collection('matches');
+  next();
+})
 
 // all environments
 app.set('port', process.env.PORT || 3000);
